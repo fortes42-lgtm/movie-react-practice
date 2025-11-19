e6a22b45302fedfdc33ff08512a42f813f171874
fortes42 <forteslv42@gmail.com>
2025-11-19 15:58:22 +0900

    chore: set up default config files for editor, eslint, prettier, and jsconfig     - Tailwind CSS 설정     - ESLint, Prettier 포맷터 및 린터 설정     - 절대 경로 설정     - PR 템플렛 추가
        - create .prettierrc
        - create jsconfig.json
        - create .vscode/settings.json
        - edit .editorconfig.js
        - edit vite.config.js
        - edit package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
+
+const __dirname = path.resolve();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils") },
+      {
+        find: "@constants",
+        replacement: path.resolve(__dirname, "src/constants"),
+      },
+      { find: "@api", replacement: path.resolve(__dirname, "src/api") },
+      { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
+      {
+        find: "@contexts",
+        replacement: path.resolve(__dirname, "src/contexts"),
+      },
+    ],
+  },
+});
